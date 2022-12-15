72f9c4331a7378876371ae6896ad8b965041bbb5
MSP17 <peachyprecy17@gmail.com>
2022-12-14 20:22:41 -0800

    task 3 -8

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+nclude "main.h"
+
+/**
+ * print_sign - functions that prints the sign of a number..
+ * @n: The input number to check
+ * Return: int.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+
+	}
+}
