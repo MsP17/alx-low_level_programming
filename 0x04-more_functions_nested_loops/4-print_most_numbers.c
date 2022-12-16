23616b86c0e553287698507e9378f0b0f9df259b
MSP17 <peachyprecy17@gmail.com>
2022-12-15 19:07:58 -0800

    task0 - task 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints
+ *
+ * Return: void/
+ */
+void print_numbers(void)
+{
+char c;
+for (!(c == '2' || c == '4'))
+_putchar(c);
+}
+_putchar('\n');
+}
